860b2ce64858bffbc5f9a80a1d2706fa739f5043
Basil <git@nadar.io>
2019-07-27 09:35:04 +0200

    closes #11

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 All notable changes to this project will be documented in this file. This project adheres to [Semantic Versioning](http://semver.org/).
 In order to read more about upgrading and BC breaks have a look at the [UPGRADE Document](UPGRADE.md).
 
+## 1.0.6
+
++ [#11](https://github.com/luyadev/luya-composer/issues/11) Replace linux paths with back slashes for windows systems.  
+
 ## 1.0.5 (14. May 2019)
 
 + [#10](https://github.com/luyadev/luya-composer/issues/10) Add new `{{DS}}` variable to fix problem with windows systems paths.
--- src/ComposerHelper.php
+++ src/ComposerHelper.php
@@ -12,12 +12,20 @@ class ComposerHelper
 {
     /**
      * Replace a string with {{DS}} chars to the current operating system directory seperator.
+     * 
+     * Or if windows system is detected replace forward slashes with back slashes.
      *
      * @param string $dir The name of the directory
      * @return string The parse string.
      */
     public static function parseDirectorySeperator($dir)
     {
+        // check if we are on a windows system
+        if (DIRECTORY_SEPARATOR === '\\') {
+            // replace forwarding slashes for linux with windows backslash
+            return str_replace('/', '\\', $dir);
+        }
+
         return str_replace([
             '{{DS}}'
         ], [
